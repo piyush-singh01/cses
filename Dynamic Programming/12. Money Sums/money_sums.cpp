ccc954750034830aa2819d7de9b4d81f085cf57c
Piyush Singh <piyush_2001cs51@iitp.ac.in>
2023-10-29 19:38:32 +0530

    init

--- Dynamic Programming/12. Money Sums/money_sums.cpp
+++ Dynamic Programming/12. Money Sums/money_sums.cpp
@@ -1,38 +1,29 @@
 #include<bits/stdc++.h>
 using namespace std;
-#define ll long long
-const int MOD = 1e9 + 7;
-int32_t main()
-{
+int32_t main() {
+    
     int n;cin>>n;
-    vector<int> a(n); for(int &i:a) cin>>i;
+    vector<int> a(n);
+    for(int &e:a)
+        cin>>e;
 
-    int x = accumulate(a.begin(), a.end(), 0);
-    vector<bool> dp(x+1);
+    int sum = accumulate(a.begin(), a.end(), 0LL);
+
+    vector<bool> dp(sum + 1, false);
     dp[0] = true;
-
-    for(int i = 1;i<=n;i++) {
-        vector<bool> new_dp(x+1);
-        new_dp[0] = true;
-        
-        for(int j = 1;j<=x  ;j++) {
-            if(j - a[i-1] < 0) {
-                new_dp[j] = dp[j]; 
-            } else {
-                new_dp[j] = dp[j] || dp[j - a[i-1]];
-            }
+    for(int i = 0;i<n;i++) {
+        for(int j = sum;j>=a[i];j--) {
+            dp[j] = dp[j] || dp[j - a[i]];
         }
-        dp = new_dp;
     }
-
     vector<int> money_sums;
-    for(int sum = 1;sum<=x;sum++) {
-        if(dp[sum]) {
-            money_sums.push_back(sum);
+    for(int i = 1;i<=sum;i++) {
+        if(dp[i]) {
+            money_sums.push_back(i);
         }
     }
-
-    cout<<(int)money_sums.size()<<endl;
+    
+    cout<<money_sums.size()<<'\n';
     for(int money:money_sums) {
         cout<<money<<" ";
     }
